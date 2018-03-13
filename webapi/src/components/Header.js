84186db5aed8bd5c33d8eab5ec7c2fc5bee8c65b
kamilnaja <kamilnaja@gmail.com>
2018-03-13 23:17:41 +0100

    routing works

--- webapi/src/components/Header.js
+++ webapi/src/components/Header.js
@@ -6,35 +6,34 @@ import {
 export const Header = (props) => {
 
     return (
-        <Router>
-            <div>
-                <header className="header">
-                    <div>
-                        <h1 className="header-site-title">
-                            {props.settings.title}
-                        </h1>
-                        <h2>{props.settings.subtitle}</h2>
-                    </div>
+        <div>
+            <header className="header">
+                <div>
+                    <h1 className="header-site-title">
+                        {props.settings.title}
+                    </h1>
+                    <h2>{props.settings.subtitle}</h2>
+                </div>
 
-                    <ul className="header-site-main-menu">
-                        <li>
-                            <Link to="/" >
-                                Home
+                <ul className="header-site-main-menu">
+                    <li>
+                        <Link to="/" >
+                            Home
                             </Link>
-                        </li>
-                        <li>
-                            <Link to="/admin">
-                                Admin Panel
+                    </li>
+                    <li>
+                        <Link to="/admin">
+                            Admin Panel
                             </Link>
-                        </li>
-                        <li>
-                            <Link to="/settings">
-                                Settings
+                    </li>
+                    <li>
+                        <Link to="/settings">
+                            Settings
                             </Link>
-                        </li>
-                    </ul>
-                </header>
-            </div>
-        </Router>
+                    </li>
+                </ul>
+            </header>
+        </div>
+
     )
 }
