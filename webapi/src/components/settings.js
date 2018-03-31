f13bbec5cc07a7bd468eed631c752edc208303ae
kamilnaja <kamilnaja@gmail.com>
2018-03-31 10:35:38 +0200

    added new start script

--- webapi/src/components/settings.js
+++ webapi/src/components/settings.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 export class Settings extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
-    }
+
     render() {
         return (
             <div>Settings</div>
