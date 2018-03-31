f70975c18cfb49fbddb56c6c44d7dcf1e3dfa1b3
kamilnaja <kamilnaja@gmail.com>
2018-03-31 09:53:42 +0200

    added sass

--- webapi/src/App.js
+++ webapi/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './styles/App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Footer } from './components/footer';
