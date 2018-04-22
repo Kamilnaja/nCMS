8bbb10b4ea3786e00092dc9aa3267c670cf8e962
kamilnaja <kamilnaja@gmail.com>
2018-04-22 23:43:41 +0200

    manage users

---
+++ webapi/src/components/adminPanel/manageUsers/manageUsers.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+class ManageUsers extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return (
+            <div>Users List</div>
+        )
+    }
+}
+
+export default ManageUsers;
