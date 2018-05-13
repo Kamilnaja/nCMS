b54e413750fecffc429fc8ac0351e1d33d035cc2
kamilnaja <kamilnaja@gmail.com>
2018-05-13 21:38:41 +0200

    added server test setup

---
+++ server/test/posts.spec.js
@@ -0,0 +1,15 @@
+var expect = require('chai').expect;
+var articleCtrl = require('./../controllers/articlesController');
+var loginCtrl = require('./../controllers/loginController');
+var settingsCtrl = require('./../controllers/settingsController');
+var userCtrl = require('./../controllers/usersController');
+
+describe('all controllers should exists', () => {
+    it('exists', () => {
+        expect(articleCtrl).to.exist;
+        expect(loginCtrl).to.exist;
+        expect(settingsCtrl).to.exist;
+        expect(userCtrl).to.exist;
+
+    })
+});
