96a6cf472ad4263c5cfe887cdc045111c8281db3
= <kamilnaja@gmail.com>
2018-11-06 09:47:36 +0100

    change case of files

--- webapi/src/components/infoStrip.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const InfoStrip = (props) => {
-    return (
-        <div className="login-info-strip">
-            {props.text} {props.user}
-        </div>
-    )
-}
